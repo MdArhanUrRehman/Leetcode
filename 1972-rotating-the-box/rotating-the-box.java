fac2c323f7e4de93a6fabc40432a821aea2f0bf7
Mohammed Arhan Ur Rehman <arhanurrehman12@gmail.com>
2026-05-07 00:36:21 +0530

    Time: 8 ms (59.38%) | Memory: 124.5 MB (91.11%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -0,0 +1,51 @@
+class Solution {
+
+     private void solve(char[] grid) {
+
+        int n = grid.length;
+        int j = n - 1;
+        boolean empty = false;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (!empty && grid[i] == '.') {
+                j = i;
+                empty = true;
+            }
+
+            else if (grid[i] == '*') {
+                j = i - 1;
+                empty = false;
+            }
+
+            else if (grid[i] == '#') {
+
+                char temp = grid[j];
+                grid[j] = grid[i];
+                grid[i] = temp;
+
+                j--;
+            }
+        }
+    }
+
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int n = boxGrid.length;
+        int m = boxGrid[0].length;
+
+        char result[][] = new char[m][n];
+
+        for(int i=0; i<n; i++){
+            solve(boxGrid[i]);
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                result[j][n-i-1] = boxGrid[i][j];
+            }
+        }
+
+        return result;
+        
+    }
+}
